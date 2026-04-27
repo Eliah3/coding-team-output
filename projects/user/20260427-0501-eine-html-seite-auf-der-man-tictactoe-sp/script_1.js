24e591d32a9c07dd90ae7f08bdfcd63d6fa29d19
Eliah3 <eliah@bisch.de>
2026-04-27 07:01:56 +0200

    feat: Eine HTML-Seite auf der man TicTacToe spielen kann

---
+++ projects/user/20260427-0501-eine-html-seite-auf-der-man-tictactoe-sp/script_1.js
@@ -0,0 +1,32 @@
+// tictactoe.js
+export function init() {
+  // ...
+}
+
+export function renderBoard() {
+  // ...
+}
+
+export function handleCellClick(e) {
+  // ...
+}
+
+export function checkWin(player) {
+  // ...
+}
+
+export function handleWin(combo) {
+  // ...
+}
+
+export function handleDraw() {
+  // ...
+}
+
+export function updateStatus(message) {
+  // ...
+}
+
+export function resetGame() {
+  // ...
+}
